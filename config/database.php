678f006ce6e51a7dfd045bc8ff5698dfbc673161
Michael Akpobome <mike98989@gmail.com>
2020-06-08 07:44:14 -0700

    updated database.php

--- config/database.php
+++ config/database.php
@@ -3,12 +3,6 @@
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 
-/*
-define('DB_NAME', 'nulaiapp_yppct');
-define('DB_USER', 'nulaiapp_root');
-define('DB_PASS', 'admin99yu76');
-*/
-
 
 define('DB_NAME', 'havilah');
 define('DB_USER', 'root');
